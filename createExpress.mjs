49a6857dafd945743f25e7f31fb8dbf65fc27f62
Pavel Pulin <Pavel.Pulin@infobip.com>
2022-11-24 12:00:30 +0100

    add client

--- createExpress.mjs
+++ createExpress.mjs
@@ -1,11 +1,9 @@
-import http from 'http';
 import express from 'express';
 
 export default function createExpress(port) {
     const app = express();
-    app.set('port', port);
-    const server = http.createServer(app);
-    server.listen(port, '0.0.0.0');
+
+    app.listen(port, () => console.log(`Server started listening to port ${port}`));
 
     return app;
 }
---
+++ requests-server.mjs
@@ -0,0 +1,33 @@
+import 'isomorphic-unfetch';
+import { createClient, fetchExchange, dedupExchange, gql } from '@urql/core';
+import createExpress from './createExpress.mjs';
+import expressStatusMonitor from 'express-status-monitor';
+
+const query = gql`
+    query {
+        books {
+            title
+            author
+        }
+    }
+`
+const urqlClient = createClient({
+    url: 'http://localhost:4000/graphql',
+    exchanges: [dedupExchange, fetchExchange],
+    requestPolicy: 'network-only',
+});
+
+
+const app = createExpress(3000);
+
+app.use(expressStatusMonitor());
+
+app.get('/', async(req, res) => {
+    const { data } = await urqlClient.query(query, { test: new Date() }).toPromise().then(result => {
+        console.log(1, result);
+
+        return result;
+    })
+
+    res.send(data)
+});
